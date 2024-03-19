bbf428ca962d3865ceca05698bc6225c3f4ca2af
Coffeecito <jdev.cafecito@gmail.com>
2024-03-19 11:39:30 -0500

    Definicion de metodos en los repositorios y creacion de la capa de servicio de las entidades

--- src/main/java/com/coffeecito/repository/CommentRepository.java
+++ src/main/java/com/coffeecito/repository/CommentRepository.java
@@ -4,6 +4,9 @@ import com.coffeecito.entity.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+    List<CommentEntity> findByPostId(Long postId);
 }
