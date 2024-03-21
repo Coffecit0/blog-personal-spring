c80e82de4ed799d5d02cdc5f4e97b4d189fc4c94
Coffeecito <jdev.cafecito@gmail.com>
2024-03-21 12:14:06 -0500

    Creacion de la vista para crear un post y configuracion de los endpoints para esta vista

--- src/main/java/com/coffeecito/controller/PostController.java
+++ src/main/java/com/coffeecito/controller/PostController.java
@@ -1,11 +1,18 @@
 package com.coffeecito.controller;
 
+import com.coffeecito.entity.PostEntity;
+import com.coffeecito.entity.UserEntity;
 import com.coffeecito.service.PostService;
+import com.coffeecito.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/post")
@@ -14,9 +21,29 @@ public class PostController {
     @Autowired
     private PostService postService;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/home")
     public String homePage(Model model) {
         model.addAttribute("posts",postService.getAllPost());
         return "/posts/home";
     }
+
+    @GetMapping("/new")
+    public String newPostPage() {
+        return "/posts/create-post";
+    }
+
+    @PostMapping("/create")
+    public String createPost(PostEntity post, HttpSession session) {
+        post.setCreatedAt(LocalDateTime.now());
+
+        UserEntity user = userService.getUserById(Long.parseLong(session.getAttribute("user_session_id").toString())).get();
+        post.setUser(user);
+
+        postService.createPost(post);
+        return "redirect:/post/home";
+    }
+
 }
