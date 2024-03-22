dbcc2520d38e0a18ea318b10dbdd6c6a82c1a35c
Coffeecito <jdev.cafecito@gmail.com>
2024-03-22 08:54:48 -0500

    Creacion de la vista para actualizar el post, configuracion de los endpoints para mapear la vista , para editar y eliminar

--- src/main/java/com/coffeecito/entity/CommentEntity.java
+++ src/main/java/com/coffeecito/entity/CommentEntity.java
@@ -20,6 +20,7 @@ public class CommentEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(columnDefinition = "TEXT")
     private String content;
     private LocalDateTime createdAt;
 
